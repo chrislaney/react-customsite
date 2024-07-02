20a6e9723dc853458b404ffa77bc9608fae312bc
chrislaney <Laneyct@mail.uc.edu>
2024-07-02 11:37:09 -0400

    worked on pagelayout more and also changed the back button icons, and fixed the file explorer icon glow

--- src/components/PageLayout/PageLayout.jsx
+++ src/components/PageLayout/PageLayout.jsx
@@ -6,16 +6,21 @@ import styles from "./PageLayout.module.css";
 
 export const PageLayout = () => {
   return (
-    <div className={styles.container}>
-        <div className={styles.leftCol}>
-            <ProfileCard></ProfileCard>
+    <div className={styles.parentContainer}>
+        <div className={styles.nameBar}>
+            Christopher Laney
         </div>
-        <div className={styles.rightCol}>
-            <div className={styles.columnElement}>
-                <GenCard></GenCard>
+        <div className={styles.container}>
+            <div className={styles.leftCol}>
+                <ProfileCard></ProfileCard>
             </div>
-            <div className={styles.columnElement}>
-                <MusicCard></MusicCard>
+            <div className={styles.rightCol}>
+                <div className={styles.columnElement}>
+                    <GenCard></GenCard>
+                </div>
+                <div className={styles.columnElement}>
+                    <MusicCard></MusicCard>
+                </div>
             </div>
         </div>
     </div>

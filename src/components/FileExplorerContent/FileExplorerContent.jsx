c9e0c9c94c0be4df9bb9c44989bee15460ff537a
chrislaney <Laneyct@mail.uc.edu>
2024-08-02 11:37:14 -0400

    Navigation partly complete. About to start on adding back buttons to file explorer followed by creating a overlay div for certain folders

--- src/components/FileExplorerContent/FileExplorerContent.jsx
+++ src/components/FileExplorerContent/FileExplorerContent.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./FileExplorerContent.module.css";
-import navoptionsjson from "../../data/navoptions.json";
+import navoptionsjson1 from "../../data/navoptions.json";
 import { getImageUrl } from "../../utils";
 
 export const FileExplorerContent = () => {
+
+  const [navOptionsJson, setNavOptionsJson] = useState(navoptionsjson1);
+
+  // Function to handle click and switch to nested JSON options or open a link
+  const handleJsonSwitch = (nextOptions) => {
+    console.log(nextOptions);
+    if (Array.isArray(nextOptions)) {
+      // If nextOptions is an array, update the state with new options
+      setNavOptionsJson(nextOptions);
+      console.log('Updated navOptionsJson:', nextOptions);
+    } else if (typeof nextOptions === 'string') {
+      // If nextOptions is a string, open the link
+      window.open(nextOptions, '_blank');
+    }
+  };
+
   return (
     <div className={styles.container}>
-
-
-
-        <div className={styles.content}>
+        <div key={navOptionsJson} className={styles.content}>
             <div className={styles.navOptionsContainer}>
-            {navoptionsjson.map((navoption, id) => {
+            {console.log('Rendering with navOptionsJson:', navOptionsJson)} {/* Debugging render */}
+            {navOptionsJson.map((navoption, id) => {
                 return (
-                <div key={id} className={styles.navOptionItem}>
+                <div key={id} className={styles.navOptionItem} onClick={() => {
+                  console.log('Item clicked'); // Check if this logs
+                  handleJsonSwitch(navoption.nextOptions);
+                }}>
                     <div className={styles.navImageContainer}>
-                    <img src={getImageUrl(navoption.imageSrc)} alt={navoption.title} />
+                    <img src={getImageUrl(navoption.imageSrc)} alt={navoption.title}  />
                     </div>
                     <p>{navoption.title}</p>
                 </div>
@@ -24,8 +41,7 @@ export const FileExplorerContent = () => {
             </div>
         </div>
 
-
-
     </div>
   )
-}
+};
+
